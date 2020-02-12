cb4c54501cba31eedd36c823b13bee9a365a97a9
sysdevguru <sysdevguru76@gmail.com>
2020-02-12 20:26:32 +0200

    initial commit

---
+++ my_app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Search
+
+
+class SearchAdmin(admin.ModelAdmin):
+    list_display = ('search', 'created')
+
+
+admin.site.register(Search, SearchAdmin)
